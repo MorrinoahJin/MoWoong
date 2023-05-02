551ed2813f9442fcf6adac85ad7e2b1632c4d6aa
JeongKiwoong <jkw4911@gmail.com>
2023-05-02 14:44:25 +0900

    공격 타이밍 재수정

---
+++ Assets/Scripts/Mori/Map/Map.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    static public int mapNum;
+    static public bool moveMap;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mapNum = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
